20565aea577b4fde1df2eee9c9828e7d7122aea3
Fani Fadillah Hermawan <fanifadilhermawan11@gmail.com>
2020-11-22 14:54:13 +0700

    recode

--- app/src/main/java/com/example/kasirapp/rest/ApiConnection.java
+++ app/src/main/java/com/example/kasirapp/rest/ApiConnection.java
@@ -5,7 +5,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiConnection {
 
-    //IP komputer atau laptop kita yang akan dijadikan server 192.168.18.2
     public static final String BASE_URL = "http://192.168.1.80/KasirLaravel/public/api/";
 
     private static Retrofit retrofit = null;
